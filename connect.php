a3eb1180426647ae82373e48a8d02a3309e2b408
ducmypham2610 <ducmypham2610@gmail.com>
2022-03-26 10:48:38 +0700

    commit connect

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-    $username = "root";
-    $password = "";
+$servername = "3.132.234.157";
+    $username = "ducmy";
+    $password = "123@123a";
     $database = "tunesource";
     $connect = new mysqli($servername,$username,$password,$database);
 ?>
